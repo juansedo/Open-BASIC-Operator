6d35ede8572bfeb1a25b7875396ad6a2c080d7b8
juansedo <JuanseDO1@users.noreply.github.com>
2019-10-11 16:20:04 -0500

    Implementación de Hash Table en C++

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+class Variables {
+private:
+    string key;
+    float* values;
+    int size;
+
+    int hashFunction(string str) {
+        int sum = 0;
+        for(int i = 0; i < sizeof(str)/sizeof(string); i++) {
+            sum += ((int)str.at(i))*i + pow(10,i);
+        }
+        return sum % size;
+    }
+public:
+    Variables(int size = 100) {
+        this->size = size;
+        values = new float[size];
+    }
+
+    void put(string k, float v) {
+        values[hashFunction(k)] = v;
+    }
+    
+    float get(string k) {
+        return values[hashFunction(k)];
+    }
+};
+
 int main() {
-    cout << "24";
+    Variables v = Variables();
+    v.put("num1", 25.0);
+
+    cout << v.get("num1");
     return 0;
 }
