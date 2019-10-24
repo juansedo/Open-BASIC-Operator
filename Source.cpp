b01f5e951354208d8178a46ed56248b5e5423799
juansedo <jsdiazo@eafit.edu.co>
2019-10-24 06:54:10 -0500

    Error de End line

--- Source.cpp
+++ Source.cpp
@@ -12,38 +12,39 @@ bool compareStr(string str1, string str2);
 
 float operar(string word, float a, float b)
 {
-	if(word == "+")
-		{
-			return a+b;
-		}
-	else if(word == "*")
-		{
-			return a*b;
-		}
-	else if(word == "-")
-		{
-			return a-b;
-		}
-	else if(word == "/")
-		{
-			return a/b;
-		}
-		else
-		{
-			return 0;
-		}
-
+	if (word == "+")
+	{
+		return a + b;
+	}
+	else if (word == "*")
+	{
+		return a * b;
+	}
+	else if (word == "-")
+	{
+		return a - b;
+	}
+	else if (word == "/")
+	{
+		return a / b;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-int main () {
+int main()
+{
 	string line;
-	bool isDeclaration;
-	bool isSentence;
-	bool thisEnded;
 
-  	ifstream myfile ("Obito.txt");
-  	if (myfile.is_open()) {
-    	getline(myfile, line);
+	cout << "Ingrese el nombre del archivo a abrir:" << endl;
+	getline(cin, line);
+	ifstream myfile("./" + line);
+
+	if (myfile.is_open())
+	{
+		getline(myfile, line);
 		Variables vars;
 		stringstream ss(line);
 		string word;
@@ -53,23 +54,27 @@ int main () {
 
 		/*It must have Begin keyword*/
 		ss >> word;
-		if (!compareStr(word, "Begin")) {
+		if (!compareStr(word, "Begin"))
+		{
 			/*If it's empty, it throws no error*/
-			if (!(word.empty())) cout << "Error: missing \"Begin\" keyword in line 01" << endl;
+			if (!(word.empty()))
+				cout << "Error: missing \"Begin\" keyword in line 01" << endl;
 			myfile.close();
 			return 0;
 		}
-		
+
 		/*Function name must have the dots at the end*/
 		ss >> word;
-		if (!(word.at(word.size()-1) == ':')) {
+		if (!(word.at(word.size() - 1) == ':'))
+		{
 			cout << "Error: expected \":\" in line 1" << endl;
 			myfile.close();
 			return 0;
 		}
 
 		/*Function name must have correct characters*/
-		if(!isFuncName(word)) {
+		if (!isFuncName(word))
+		{
 			cout << "Error: bad name of function in line 1" << endl;
 			myfile.close();
 			return 0;
@@ -77,7 +82,8 @@ int main () {
 
 		/*It must have no more words*/
 		ss >> word;
-		if (ss) {
+		if (ss)
+		{
 			cout << "Error: too many arguments" << endl;
 			myfile.close();
 			return 0;
@@ -86,28 +92,33 @@ int main () {
 		ss.str("");
 		ss.clear();
 
-		for (int i = 2; getline (myfile, line); i++) {
-      		/*Line is put into a stream*/
+		for (int i = 2; getline(myfile, line); i++)
+		{
+			/*Line is put into a stream*/
 			ss << line;
 			ss >> word;
-			if (compareStr(word, "//") || compareStr(word.substr(0,2), "//")) {
+			if (compareStr(word, "//") || compareStr(word.substr(0, 2), "//"))
+			{
 				ss.str("");
 				ss.clear();
 				continue;
 			}
 			string newVariable;
-			if (word.at(0) == '@') {
+			if (word.at(0) == '@')
+			{
 				/************************DECLARATION**************************/
 				//@var <= Num
 				/*Name of variable*/
 				newVariable = word.substr(1);
-				if (compareStr(newVariable, "Num")) {
+				if (compareStr(newVariable, "Num"))
+				{
 					cout << "Error: Num is an OBO keyword, use another name in line " << i << endl;
 					myfile.close();
 					return 0;
 				}
 
-				if (!isVarName(newVariable)) {
+				if (!isVarName(newVariable))
+				{
 					cout << "Error: incorrect name in line " << i << endl;
 					myfile.close();
 					return 0;
@@ -115,7 +126,8 @@ int main () {
 
 				/*Assignation operar*/
 				ss >> word;
-				if (!compareStr(word, "<=")) {
+				if (!compareStr(word, "<="))
+				{
 					cout << "Error: expected \"<=\" in line " << i << endl;
 					myfile.close();
 					return 0;
@@ -123,7 +135,8 @@ int main () {
 
 				/*Data type*/
 				ss >> word;
-				if (!compareStr(word, "Num")) {
+				if (!compareStr(word, "Num"))
+				{
 					cout << "Error: unknown data type in line " << i << endl;
 					myfile.close();
 					return 0;
@@ -131,79 +144,96 @@ int main () {
 				/*Variable created*/
 				vars.put(newVariable, 0);
 			}
-			else if (word.at(0) == '~') {
+			else if (word.at(0) == '~')
+			{
 				/************************PRINT OR READ FUNCTION***************/
 				//~Print var
 				//~Print 2
 				//~Read
 				/*Print function comprobation*/
-				if (compareStr(word.substr(1), "Print")) {
+				if (compareStr(word.substr(1), "Print"))
+				{
 					ss >> word;
-					try {
+					try
+					{
 						float f = stof(word);
 						cout << stof(word) << endl;
 					}
-					catch (invalid_argument& e) {
-						try {
+					catch (invalid_argument &e)
+					{
+						try
+						{
 							float f = vars.get(word);
 							cout << f << endl;
 						}
-						catch (const char* e) {
+						catch (const char *e)
+						{
 							cout << "Error: " << word << " was not declared, line " << i << endl;
 							myfile.close();
 							return 0;
 						}
 					}
 				}
-				
-				else if (compareStr(word.substr(1), "Read")) {
+
+				else if (compareStr(word.substr(1), "Read"))
+				{
 					string aux;
 					cin >> aux;
 				}
 
-				else {
+				else
+				{
 					cout << "Warning: unknown function in line " << i << endl;
 					continue;
 				}
 			}
-			else {
-				if (!ss) {
+			else
+			{
+				if (!ss)
+				{
 					ss.str("");
 					ss.clear();
 					continue;
+				}
+				
+				if(compareStr(word, "End")) {
+					cout << "End line reached!" << endl;
+					break;
 				}
 				/************************ASSIGNATION**************************/
 				//var = 5 + 2
 				//num = var + 3
 				/*Name of variable to assign*/
 				string var_to_assign;
-				try {
+				try
+				{
 					vars.get(word);
 					var_to_assign = word;
 				}
-				catch (const char* e) {
+				catch (const char *e)
+				{
 					cout << e << " in line " << i << endl;
 					myfile.close();
 					return 0;
 				}
-				
+
 				/*Assignation operar*/
 				ss >> word;
-				if (!compareStr(word, "=")) {
+				if (!compareStr(word, "="))
+				{
 					cout << "Error: expected \"=\" in line " << i << endl;
 					myfile.close();
 					return 0;
 				}
 
-				/*KY*/
-        ss >> word;
-				vector <float> numeros;
-				vector <string> operadores;
-				while(ss)
-				{	
-					if(isVarName(word))
-					{ 
-						if(numeros.size() > 0)
+				ss >> word;
+				vector<float> numeros;
+				vector<string> operadores;
+				while (ss)
+				{
+					if (isVarName(word))
+					{
+						if (numeros.size() > 0)
 						{
 							float temp = numeros.back();
 							numeros.pop_back();
@@ -214,15 +244,14 @@ int main () {
 						{
 							numeros.push_back(vars.get(word));
 						}
-						
 					}
 					else if (isdigit(word[0]))
 					{
 						try
 						{
-							if(numeros.size() > 0)
+							if (numeros.size() > 0)
 							{
-								
+
 								float temp = numeros.back();
 								numeros.pop_back();
 								numeros.push_back(operar(operadores.back(), temp, stof(word)));
@@ -233,19 +262,19 @@ int main () {
 								numeros.push_back(stof(word));
 							}
 						}
-						catch(exception e)
+						catch (exception e)
 						{
-							cout << "Error, not a nunmber in line " << i << endl;
+							cout << "Error, not a number in line " << i << endl;
 							return 0;
 						}
 					}
-					else if(word[0] == '~')
+					else if (word[0] == '~')
 					{
-						if(compareStr(word.substr(1), "Read"))
+						if (compareStr(word.substr(1), "Read"))
 						{
 							float a;
 							cin >> a;
-							if(numeros.size() > 0)
+							if (numeros.size() > 0)
 							{
 								float temp = numeros.back();
 								numeros.pop_back();
@@ -262,12 +291,12 @@ int main () {
 							cout << "Error: invalid function in line " << i << endl;
 						}
 					}
-					else if(word == "(")
+					else if (word == "(")
 					{
 						numeros.push_back(1);
 						operadores.push_back("*");
 					}
-					else if(word == ")")
+					else if (word == ")")
 					{
 						float temp = numeros.back();
 						numeros.pop_back();
@@ -276,103 +305,304 @@ int main () {
 						numeros.push_back(operar(operadores.back(), temp2, temp));
 						operadores.pop_back();
 					}
-					else if(word == "+" || word == "-" || word == "*" || word == "/")
+					else if (word == "+" || word == "-" || word == "*" || word == "/")
 					{
 						operadores.push_back(word);
 					}
-          			ss >> word;
-
+					ss >> word;
 				}
 				vars.put(var_to_assign, numeros.back());
-			   	}
-						ss.str("");
-						ss.clear();
-					}
-					myfile.close();
-				}
-				else cout << "Error: unable to open file"; 
-				system("pause");
-				return 0;
-
-				
 			}
+			ss.str("");
+			ss.clear();
+		}
+		myfile.close();
+	}
+	else
+		cout << "Error: unable to open file";
+	system("pause");
+	return 0;
+}
 
-
-
-bool isFuncName(string str) {
-	for (char c: str) {
-		if (c == str.at(0)) {
-			switch(c) {
-				case '0': case '1': case '2': case '3': case '4': case '5':
-				case '6': case '7': case '8': case '9':
-				case '|': case '!': case '#': case '$': case '%': case '&':
-            	case '(': case ')': case '[': case ']': case '{': case '}': 
-				case '.': case '<': case '>': case ':': case ';': case '/':
-				case '\\': case '?':case '@': case '*': case '=': case '+':
-				case '^': case '`': case ',': case '-': case '~':
+bool isFuncName(string str)
+{
+	for (char c : str)
+	{
+		if (c == str.at(0))
+		{
+			switch (c)
+			{
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			case '|':
+			case '!':
+			case '#':
+			case '$':
+			case '%':
+			case '&':
+			case '(':
+			case ')':
+			case '[':
+			case ']':
+			case '{':
+			case '}':
+			case '.':
+			case '<':
+			case '>':
+			case ':':
+			case ';':
+			case '/':
+			case '\\':
+			case '?':
+			case '@':
+			case '*':
+			case '=':
+			case '+':
+			case '^':
+			case '`':
+			case ',':
+			case '-':
+			case '~':
 				return false;
 			}
 		}
-		switch(c) {
-				case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
-                case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
-                case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
-                case 'V': case 'W': case 'X': case 'Y': case 'Z':
-                case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
-                case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
-                case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
-                case 'v': case 'w': case 'x': case 'y': case 'z':
-				case '0': case '1': case '2': case '3': case '4': case '5':
-				case '6': case '7': case '8': case '9':
-				case '!': case '#': case '$': case '%': case '&':
-            	case '[': case ']': case '{': case '}': 
-				case '<': case '>': case ':': case '/': case '\\': case '?':
-				case '@': case '*': case '+': case '^': case '_':
-				case '`': case '-': case '~':
-					break;
-				default:
-					return false;
-			}
+		switch (c)
+		{
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+		case 'G':
+		case 'H':
+		case 'I':
+		case 'J':
+		case 'K':
+		case 'L':
+		case 'M':
+		case 'N':
+		case 'O':
+		case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+		case 'T':
+		case 'U':
+		case 'V':
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'g':
+		case 'h':
+		case 'i':
+		case 'j':
+		case 'k':
+		case 'l':
+		case 'm':
+		case 'n':
+		case 'o':
+		case 'p':
+		case 'q':
+		case 'r':
+		case 's':
+		case 't':
+		case 'u':
+		case 'v':
+		case 'w':
+		case 'x':
+		case 'y':
+		case 'z':
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case '!':
+		case '#':
+		case '$':
+		case '%':
+		case '&':
+		case '[':
+		case ']':
+		case '{':
+		case '}':
+		case '<':
+		case '>':
+		case ':':
+		case '/':
+		case '\\':
+		case '?':
+		case '@':
+		case '*':
+		case '+':
+		case '^':
+		case '_':
+		case '`':
+		case '-':
+		case '~':
+			break;
+		default:
+			return false;
+		}
 	}
 	return true;
 }
 
-bool isVarName(string str) {
-	for (char c: str) {
-		if (c == str.at(0)) {
-			switch(c) {
-				case '0': case '1': case '2': case '3': case '4': case '5':
-				case '6': case '7': case '8': case '9':
-				case '|': case '!': case '#': case '$': case '%': case '&':
-            	case '(': case ')': case '[': case ']': case '{': case '}': 
-				case '.': case '<': case '>': case ':': case ';': case '/':
-				case '?': case '@': case '*': case '=': case '+':
-				case '^': case '`': case ',': case '-': case '~':
+bool isVarName(string str)
+{
+	for (char c : str)
+	{
+		if (c == str.at(0))
+		{
+			switch (c)
+			{
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			case '|':
+			case '!':
+			case '#':
+			case '$':
+			case '%':
+			case '&':
+			case '(':
+			case ')':
+			case '[':
+			case ']':
+			case '{':
+			case '}':
+			case '.':
+			case '<':
+			case '>':
+			case ':':
+			case ';':
+			case '/':
+			case '?':
+			case '@':
+			case '*':
+			case '=':
+			case '+':
+			case '^':
+			case '`':
+			case ',':
+			case '-':
+			case '~':
 				return false;
 			}
 		}
-		switch(c) {
-				case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
-                case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
-                case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
-                case 'V': case 'W': case 'X': case 'Y': case 'Z':
-                case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
-                case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
-                case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
-                case 'v': case 'w': case 'x': case 'y': case 'z':
-				case '0': case '1': case '2': case '3': case '4': case '5':
-				case '6': case '7': case '8': case '9':
-				case '!': case '#': case '$': case '<': case '>': case ':':
-				case '/': case '?': case '_': case '`': case '~':
-					break;
-				default:
-					return false;
-			}
+		switch (c)
+		{
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+		case 'G':
+		case 'H':
+		case 'I':
+		case 'J':
+		case 'K':
+		case 'L':
+		case 'M':
+		case 'N':
+		case 'O':
+		case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+		case 'T':
+		case 'U':
+		case 'V':
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'g':
+		case 'h':
+		case 'i':
+		case 'j':
+		case 'k':
+		case 'l':
+		case 'm':
+		case 'n':
+		case 'o':
+		case 'p':
+		case 'q':
+		case 'r':
+		case 's':
+		case 't':
+		case 'u':
+		case 'v':
+		case 'w':
+		case 'x':
+		case 'y':
+		case 'z':
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case '!':
+		case '#':
+		case '$':
+		case '<':
+		case '>':
+		case ':':
+		case '/':
+		case '?':
+		case '_':
+		case '`':
+		case '~':
+			break;
+		default:
+			return false;
+		}
 	}
 
 	return true;
 }
 
-bool compareStr(string str1, string str2) {
+bool compareStr(string str1, string str2)
+{
 	return str1.compare(str2) == 0;
 }
